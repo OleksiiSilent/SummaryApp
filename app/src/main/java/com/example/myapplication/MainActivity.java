0d4c65fd5cb5c5aa1aeb8042eba194a18b492453
OleksiiSilent <anarbutovich@gmail.com>
2024-01-19 21:29:42 +0200

    add main layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
+    }
+
+    public void onClick(View view){
+        Intent intent;
+        int id = view.getId();
+        if (id == R.id.currency_calc_btn) {
+            intent = new Intent("CurrencyCalculatorActivity");
+        } else if (id == R.id.currency_list_btn) {
+            intent = new Intent("CurrencyRateActivity");
+        } else if (id == R.id.tip_calculator) {
+            intent = new Intent("TipCalculatorActivity");
+        } else if (id == R.id.about) {
+            intent = new Intent("InfoActivity");
+        } else {
+            intent = new Intent("MainActivity");
+        }
+        startActivity(intent);
     }
 }
