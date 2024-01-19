ef36ed90b0630821b3133305c2e81a5b7c04f829
OleksiiSilent <anarbutovich@gmail.com>
2024-01-20 00:10:38 +0200

    Migrate to fragments

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,23 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main_activity);
-    }
-
-    public void onClick(View view){
-        Intent intent;
-        int id = view.getId();
-        if (id == R.id.currency_calc_btn) {
-            intent = new Intent("CurrencyCalculatorActivity");
-        } else if (id == R.id.currency_list_btn) {
-            intent = new Intent("CurrencyRateActivity");
-        } else if (id == R.id.tip_calculator) {
-            intent = new Intent("TipCalculatorActivity");
-        } else if (id == R.id.about) {
-            intent = new Intent("InfoActivity");
-        } else {
-            intent = new Intent("MainActivity");
-        }
-        startActivity(intent);
+        setContentView(R.layout.activity_main);
     }
 }
